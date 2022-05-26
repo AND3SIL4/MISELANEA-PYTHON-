04d74e3da455f3fa999bba64739b4c3d0475ff22
FELIPE SILVA  FELIPE <104874345+AND3SIL4@users.noreply.github.com>
2022-05-26 04:00:35 +0000

    FAFA

--- main.py
+++ main.py
@@ -1,43 +1,35 @@
-print("MISELANEA EJERCICIOS PROGRAMACION PYTHON \n")
-print("Menú: \n")
-print("1) Operadores")
-print("2) Condicionales")
-print("3) Ciclos \n")
+print("MISELANEA EJERCICIOS PROGRAMACION PYTHON \n  Menú: \n   1) Operadores \n   2) Condicionales \n   3) Ciclos \n   4) Arreglos \n")
 print("Ingrese la opcion que desee:")
 numero = int(input())
-print("---------------------------------------------------------------- \n")
-if (numero == 1):
-    print("Bienvenido a operadores \n")
-    print("Menu \n")
-    print("1) Calcular el area de un triangulo.")
-    print("2) Suma de dos numeros.")
-    print("3) Número elevado al cuadrado.")
-    print("4) Convierta euros a dolares.")
-    print("5) Valor del area y perimetro de un cuadrado")
-    print("6) Area y volumen de un cilindro.")
-    print("7) Longitud y area de un circulo.")
-    print("8) Promedio de tres numeros.")
-    print("99) Regresar. \n")
+print("============================================================================ \n")
+while (numero == 1):
+    print("Bienvenido a operadores \n Menu \n  1) Calcular el area de un triangulo.\n  2) Suma de dos numeros.\n  3) Número elevado al cuadrado.\n  4) Convierta euros a dolares.\n  5) Valor del area y perimetro de un cuadrado\n  6) Area y volumen de un cilindro.\n  7) Longitud y area de un circulo.\n  8) Promedio de tres numeros.\n  99) Regresar. \n")
     numeroO = int(input("Ingrese la opcion que quiera: "))
-    print("---------------------------------------------------------------\n")
-    if (numeroO == 1):
+    print("=========================================================================\n")
+    if (numeroO==99):
+      print (">>> Programa terminado <<<")
+      break 
+    elif (numeroO == 1):
         print("Calcular el area de un triangulo.")
         base = float(input("Ingrese el valor de la base del triangulo: \n"))
         altura = float(
             input("Ingrese el valor de la altura del triangulo: \n"))
         resultado = base * altura / 2
         print("El area es:" + str(resultado))
+        print ("\n============================================================================")
     elif (numeroO == 2):
         print("Suma de dos numeros \n")
         numero1 = float(input("ingrese el primer número \n"))
         numero2 = float(input("ingrese el segundo número \n"))
         resultado = numero1 + numero2
         print("resultado: " + str(resultado))
+        print ("\n============================================================================")
     elif (numeroO == 3):
         print("Número elevado al cuadrado.")
         numero = float(input("Ingrese el numero: \n \n"))
         resultado = numero**2
         print("La potencia es: " + str(resultado))
+        print ("\n============================================================================")
     elif (numeroO == 4):
         print("Conversion de Euros a Dolares \n")
         valor = float(input("ingrese el valor a convertir: \n \n"))
@@ -45,6 +37,7 @@ if (numero == 1):
         dolar = 1.06
         conversion = valor * dolar / euro
         print("El valor convertido es: " + str(conversion))
+        print ("\n============================================================================")
     elif (numeroO == 5):
         print("Calcular el area y perimetro de un cuadrado \n \n")
         lado = float(input("Ingrese el valor del lado del cuadrado: \n"))
@@ -52,6 +45,7 @@ if (numero == 1):
         print("El area del cuadrado es: " + str(area))
         perimetro = lado + lado + lado + lado
         print("El perimetro del cuadrado es: " + str(perimetro))
+        print ("\n============================================================================")
     elif (numeroO == 6):
         print(
             "Calcula el area y el volumen de un cilindro en centímetros \n \n")
@@ -63,10 +57,9 @@ if (numero == 1):
         print("El area del cilindro en centímetros cuadrados es: " + str(area))
         print("El volumen del cilindro en centímetros cúbicos es: " +
               str(volumen))
+        print ("\n============================================================================")
     elif (numeroO == 7):
-        print(
-            "Calcula la longitud y area darea de una circunferencia en centímetros \n"
-        )
+        print("Calcula la longitud y area darea de una circunferencia en centímetros \n")
         radio = float(
             input("Ingrese el valor del radio de la circunferencia: \n"))
         pi = 3.141592
@@ -74,6 +67,7 @@ if (numero == 1):
         area = pi * radio**2
         print("La longitud de la circunferencia es: " + str(longitud))
         print("El area de la circunferencia es: " + str(area))
+        print ("\n============================================================================")
     elif (numeroO == 8):
         print("Calcular el promedio de tres números")
         numero1 = float(input("Ingrese el primer número: \n"))
@@ -81,25 +75,16 @@ if (numero == 1):
         numero3 = float(input("Ingrese el tercer número: \n"))
         promedio = numero1 + numero2 + numero3
         resultado = promedio / 3
-        print("El promedio de los tres números es: " + str(resultado))
-    else:
-        (numeroO == 99)
-if (numero == 2):
-    print("Bienvenido a condicionales")
-    print("Menu")
-    print("1) Saber si el número ingresado es positivo o negativo")
-    print("2) Cual número es mayor y cual es menor")
-    print("3) Numeros mayor y menor entre tres numeros dados")
-    print("4) Si A es menor que B se suman, de lo contrario se restan")
-    print("5) Cociente entre A y B")
-    print(
-        "6) Sumar dos numeros A y B si alguno de ellos son negativos, de lo contrario se multiplican"
-    )
-    print("7) Determinar si un año es bisiesto")
-    print("99) Devolverse")
-    number = int(input("Elija la opcion que quiere: "))
-    print("---------------------------------------------------------------\n")
-    if (number == 1):
+        print("El promedio de los tres números es: " + str(resultado)) 
+        print ("\n============================================================================")
+while (numero == 2):
+  print("Bienvenido a condicionales \n Menu \n  1) Saber si el número ingresado es positivo o negativo \n  2) Cual número es mayor y cual es menor \n  3) Numeros mayor y menor entre tres numeros dados \n  4) Si A es menor que B se suman, de lo contrario se restan \n  5) Cociente entre A y B \n  6) Sumar dos numeros A y B si alguno de ellos son negativos, de lo contrario se multiplican \n  7) Determinar si un año es bisiesto\n  99) Terminar \n")
+  number2 = int(input("Elija la opcion que quiere: "))
+  print("=========================================================================\n")
+  if (number2==99):
+      print (">>> Programa Terminado <<<")
+      break
+  elif (number2 == 1):
         print("¿El número es negativo o positivo? \n Escribe el número:")
         numero = int(input())
         if (numero > 0):
@@ -108,7 +93,8 @@ if (numero == 2):
             print("El número es negativo ")
         else:
             print("El número es igual a cero ")
-    elif (number == 2):
+        print ("\n============================================================================")
+  elif (number2 == 2):
         print("¿Que número es menor y que número es mayor? \n")
         numeroUno = float(input("Escriba el primer número: \n"))
         numeroDos = float(input("Escriba el segundo número: \n"))
@@ -120,7 +106,8 @@ if (numero == 2):
             print("El número menor es: " + str(numeroUno))
         else:
             print("Ambos números son iguales ")
-    elif (number == 3):
+        print ("\n============================================================================")
+  elif (number2 == 3):
         print(
             "Calcula el número mayor y el número menor entre tres numeros \n")
         numeroUno = float(input("Ingrese el primer número: \n"))
@@ -131,7 +118,8 @@ if (numero == 2):
         menor = min(thislist)
         print("El numero mayor es: " + str(mayor))
         print("El numero menor es: " + str(menor))
-    elif (number == 4):
+        print ("\n============================================================================")
+  elif (number2 == 4):
         print("Si A es menor que B se suman, de lo contrario se restan \n \n")
         numeroUno = float(input("Ingrese el primer número; A: \n"))
         numeroDos = float(input("Ingrese el segundo número; B: \n"))
@@ -143,7 +131,8 @@ if (numero == 2):
             print("La resta es: " + str(resta))
         else:
             print("No se opera porque ambos números son iguales")
-    elif (number == 5):
+        print ("\n============================================================================")
+  elif (number2 == 5):
         print("Calcula el cociente entre A y B \n")
         primerNumero = float(input("Ingrese el número A: \n"))
         segundoNumero = float(input("Ingrese el número B: \n"))
@@ -159,10 +148,9 @@ if (numero == 2):
             (primerNumero == segundoNumero)
             igualdad = primerNumero / segundoNumero
             print("El resultado de la división es: " + str(igualdad))
-    elif (number == 6):
-        print(
-            "Dados dos numeros, A y B. Si alguno de ellos es negativo se suman, de lo contrario se multiplican \n"
-        )
+        print ("\n============================================================================")
+  elif (number2 == 6):
+        print("Dados dos numeros, A y B. Si alguno de ellos es negativo se suman, de lo contrario se multiplican \n")
         numeroUno = float(input("Ingrese el número A: \n"))
         numeroDos = float(input("Ingrese el número B: \n"))
         if (numeroUno >= 0) and (numeroDos >= 0):
@@ -179,7 +167,8 @@ if (numero == 2):
             (numeroUno > 0) and (numeroDos < 0)
             suma = numeroUno + numeroDos
             print("El resultado de la suma es: " + str(suma))
-    elif (number == 7):
+        print ("\n============================================================================")
+  elif (number2 == 7):
         print("Determinar si el año ingresado es bisiesto o no: \n")
         year = int(input("Ingrese el año: \n"))
         division = year / 4
@@ -189,29 +178,15 @@ if (numero == 2):
             print("El año SI es bisiesto \n\n")
         else:
             print("El año NO es bisiesto \n\n")
-    else:
-        (number == 99)
-
-if (numero == 3):
-    print("Bienvenido a ciclos")
-    print("Menu")
-    print("1) Multiplos de 3 entre 1 - 100")
-    print("2) Numeros impares entre 0 - 100")
-    print("3) Numeros pares del 1 - 100")
-    print("4) Mostrar en pantalla los cuadrados de los numeros del 1 - 30")
-    print(
-        "5) Sumar los cuadrados de los 100 primeros numeros naturales y mostrar el resultado en pantalla"
-    )
-    print(
-        "6) Dados dos numeros naturales, el primero menor que el segundo, mostar todos los numeros comprendidos entre ellos en secuencia acendente"
-    )
-    print(
-        "7) Sumar todos los numeros que se digitan por teclado mientras que no sea cero"
-    )
-    print("99) Volver")
-    ciclo = int(input("Elija la opcion que quiere: "))
-    print("----------------------------------------------------------------\n")
-    if (ciclo == 1):
+        print ("\n============================================================================")
+while (numero == 3):
+    print("Bienvenido a ciclos \n Menu \n  1) Multiplos de 3 entre 1 - 100 \n  2) Numeros impares entre 0 - 100 \n  3) Numeros pares del 1 - 100 \n  4) Mostrar en pantalla los cuadrados de los numeros del 1 - 30 \n  5) Sumar los cuadrados de los 100 primeros numeros naturales y mostrar el resultado en pantalla \n  6) Dados dos numeros naturales, el primero menor que el segundo, mostar todos los numeros comprendidos entre ellos en secuencia acendente \n  7) Sumar todos los numeros que se digitan por teclado mientras que no sea cero \n  99) Volver \n")
+    ciclo = int(input("Elija la opcion que quiere: ")) 
+    print("===========================================================================")
+    if (ciclo == 99):
+      print (">>> Programa Terminado <<<")
+      break 
+    elif (ciclo == 1):
         print("Multiplos del número 3, entre 1 - 100")
         def multiplo(valor, multiplo):
           return True if valor % multiplo == 0 else False
@@ -220,6 +195,7 @@ if (numero == 3):
           if multiplo(x, 3):
             multipla.append(x)
         print("Los multiplos de 3 son:", multipla)
+        print ("\n============================================================================")
     if (ciclo == 2):
         print("Numeros impares entre 0 - 100")
         numeros = []
@@ -229,9 +205,11 @@ if (numero == 3):
           if numero % 2 != 0:
             numeros.append(numero)
         print("Los numeros impares son: " + str(numeros))
+        print ("\n============================================================================")
     if (ciclo == 3):
         print("Numeros pares del 1 - 100 son: ")
         print([i for i in range(1,101) if i%2==0])
+        print ("\n============================================================================")
     elif (ciclo == 4):
         print("Cuadrados de los numeros del 1 - 30")
         def generarCuadrados(n):
@@ -242,6 +220,7 @@ if (numero == 3):
             raise ValueError("n, no debe ser mayor a 2*n")
         resultado = generarCuadrados(15)
         print("Los cuadrados son:", resultado)
+        print ("\n============================================================================")
     elif (ciclo == 5):
         print("Sumar los cuadrados de los 100 primeros numeros naturales y mostrar el resultado en pantalla. \nLos cuadrados son: \n")
         suma = 0
@@ -250,6 +229,7 @@ if (numero == 3):
             suma = suma + cuadrado
             print(f"El cuadrado de {i+1} = {cuadrado}")
             print("La suma es: ", suma)
+            print("\n============================================================================")
     elif (ciclo == 6):
         print("Dados dos numeros naturales, el primero menor que el segundo, mostar todos los numeros comprendidos entre ellos en secuencia acendente")
         n1=int(input("Ingrese el primer numero: "))
@@ -263,10 +243,9 @@ if (numero == 3):
         else:
           (n1>=n2)
           print("ERROR... el segundo numero tiene que ser mayor que el primero")
+          print ("\n============================================================================")
     elif (ciclo == 7):
-        print(
-            "Sumar todos los numeros que se digitan por teclado mientras que no sea cero"
-        )
+        print("Sumar todos los numeros que se digitan por teclado mientras que no sea cero")
         condicion = True
         suma = 0
         while condicion:
@@ -276,5 +255,33 @@ if (numero == 3):
             if f == 0:
                 print("No se puede sumar el número cero (0)")
                 condicion = False
-            else:
-              print("99) Volver")
+        print ("\n============================================================================")
+
+while (numero == 4):
+  print ("Bienvenido a arreglos \n Menu \n  1) Generar numeros aleatorios y ordenarlos con el metodo burbuja \n  2) Introducir 20 numeros enteros y ordenarlos al inverso \n  3) 10 Valores enteros ordenados en tres formas distintas \n  4) Encontrar el numero mayor y menor y su posicion original \n  5) Histograma de numeros \n  6) Introducir un numero y mostrar cuantas veces se repite en un arreglo \n  7) Mostrar 1 para los numeros primos y  0 para los que no son primos entre 8 numeros diferentes \n")
+  arreglo=int(input("Ingrese una opcion: "))
+  print("============================================================================= \n")
+  if arreglo == 99:
+    print (">>> Programa Terminado <<<")
+    break 
+  elif arreglo == 1:
+    print ("")
+    print("============================================================================= \n")
+  elif arreglo == 2:
+    print ("")
+    print("============================================================================= \n")
+  elif arreglo == 3:
+    print ("")
+    print("============================================================================= \n")
+  elif arreglo == 4:
+    print ("")
+    print("============================================================================= \n")
+  elif arreglo == 5:
+    print ("")
+    print("============================================================================= \n")
+  elif arreglo == 6:
+    print ("")
+    print("============================================================================= \n")
+  elif arreglo == 7:
+    print ("")
+    
